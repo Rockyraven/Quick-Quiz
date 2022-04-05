3645aa0ce57c7323f385228f884ff3723cb4321c
Rockyraven <89784704+Rockyraven@users.noreply.github.com>
2022-04-05 10:51:59 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
+import { QuestionProvider } from 'context/QuestionContext'
 import React from 'react'
 import  ReactDOM  from 'react-dom'
 import { App } from './App'
 import './index.css'
 
-ReactDOM.render(<App/>,document.getElementById('root'))
+ReactDOM.render(
+<QuestionProvider>
+
+<App/>
+</QuestionProvider>
+
+
+,document.getElementById('root'))
