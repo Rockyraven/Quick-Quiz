6b6e1783e30924c31110761fe4ba345133db368b
Rockyraven <89784704+Rockyraven@users.noreply.github.com>
2022-04-15 11:57:30 +0530

    fixed bugs

--- src/component/card/QuizCard.js
+++ src/component/card/QuizCard.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 export const QuizCard = ({title, image,_id}) => {
   return (
     <div className='quiz-card-container'>
-        <img src={image} alt="" />
+        <img src={image} alt="quizcard" />
         <p className='card-heading'>{title}</p>
         <Link to={`/guideline/${_id}`} className='play-button'>Play Now</Link>
     </div>
--- src/component/categories/CategoryQuiz.jsx
+++ src/component/categories/CategoryQuiz.jsx
@@ -5,7 +5,7 @@ import { QuizCard } from 'component/card/QuizCard'
 export const CategoryQuiz = () => {
   return (
   <>
-      {categories.map(category => <QuizCard title={category.name} _id={category._id} image={category.image}/>)}
+      {categories.map(category => <QuizCard key={category._id} title={category.name} _id={category._id} image={category.image}/>)}
   </>
   )
 }
