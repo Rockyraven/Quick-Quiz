edc365dae59c913fccf0e056025c4d46fb25ced2
Rockyraven <89784704+Rockyraven@users.noreply.github.com>
2022-04-17 15:27:53 +0530

    file format added

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,11 +1,10 @@
-import category1 from './category1.svg'
-import math from './math-image.jpg'
-import politics from './politics.jpg'
-import history from './history.jpg'
-import sports from './sports.jpg'
-import guideline from './guidline.jpg'
-import animal from './animal.jpg'
-import gk from './GK.jpg'
+import category1 from "./category1.svg";
+import math from "./math-image.jpg";
+import politics from "./politics.jpg";
+import history from "./history.jpg";
+import sports from "./sports.jpg";
+import guideline from "./guidline.jpg";
+import animal from "./animal.jpg";
+import gk from "./GK.jpg";
 
-
-export {category1, math, politics, history, sports, guideline,animal, gk }
+export { category1, math, politics, history, sports, guideline, animal, gk };
--- src/db/category-db.js
+++ src/db/category-db.js
@@ -1,40 +1,48 @@
 import { v4 as uuid } from "uuid";
-import {category1, math, politics, history, sports,animal, gk } from '../assets'
-export const categories = [ 
-    {
+import {
+  math,
+  politics,
+  history,
+  sports,
+  animal,
+  gk,
+} from "../assets";
+
+export const categories = [
+  {
     _id: uuid(),
     name: "Mathmatics",
     image: math,
-    link: "https://opentdb.com/api.php?amount=10&category=19&difficulty=medium&type=multiple"
-    },
-    {
+    link: "https://opentdb.com/api.php?amount=10&category=19&difficulty=medium&type=multiple",
+  },
+  {
     _id: uuid(),
     name: "Sports",
     image: sports,
-    link: "https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple"
-    },
-    {
+    link: "https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple",
+  },
+  {
     _id: uuid(),
     name: "History",
     image: history,
-    link: "https://opentdb.com/api.php?amount=10&category=23&difficulty=easy&type=multiple"
-    },
-    {
+    link: "https://opentdb.com/api.php?amount=10&category=23&difficulty=easy&type=multiple",
+  },
+  {
     _id: uuid(),
     name: "Politics",
     image: politics,
-    link: "https://opentdb.com/api.php?amount=10&category=24&difficulty=medium&type=multiple"
-    },
-    {
+    link: "https://opentdb.com/api.php?amount=10&category=24&difficulty=medium&type=multiple",
+  },
+  {
     _id: uuid(),
     name: "Animal",
     image: animal,
-    link: "https://opentdb.com/api.php?amount=10&category=27&difficulty=easy&type=multiple"
-    },
-    {
+    link: "https://opentdb.com/api.php?amount=10&category=27&difficulty=easy&type=multiple",
+  },
+  {
     _id: uuid(),
     name: "GK",
     image: gk,
-    link: "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple"
-    }
-]
+    link: "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple",
+  },
+];
